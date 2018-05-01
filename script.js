c18e6a28ae9018e23ea4c2ad536194b0941592b7
Joseph Cardillo <Cardillo@Josephs-iMac.fios-router.home>
2018-05-01 06:44:13 -0400

    learned v-bind, v-html, and v-once.

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 new Vue({
   el: '#app',
   data: {
-    title: 'Hello World!'
+    title: 'Hello World!',
+    link: 'http://google.com',
+    finishedLink: '<a href="http://google.com">Google</a>'
   },
   methods: {
     changeTitle: function(event) {
+      // this calls the data object
       this.title = event.target.value;
     },
     sayHello: function() {
-      return 'Hello!';
+      this.title = 'Hello!';
+      return this.title;
     }
   }
 });
