4bcdd077b006dec7ceb9dec34ccc83ea712f8c90
Joseph Cardillo <Cardillo@Josephs-iMac.fios-router.home>
2018-05-06 06:08:07 -0400

    watch property

--- script.js
+++ script.js
@@ -9,9 +9,29 @@ new Vue({
     counter2: 0,
     counter3: 0,
     counter4: 0,
+    secondCounter4: 0,
     x: 0,
     y: 0,
     name: 'Joe'
+  },
+  // for running synchronous tasks:
+  computed: {
+    // used like properties in the DOM, just like the data object
+    output4: function() {
+      // console.log('computed')
+      return this.secondCounter4 > 5 ? 'Greater than 5' : 'Smaller than 5';
+    }
+  },
+  // for running asynchronous tasks:
+  watch: {
+    // To reset the counter to 0 after two seconds:
+    counter4: function(value) {
+      // must store vue instance in separate variable since we're calling it in a callback closure
+      var vm = this;
+      setTimeout(function() {
+        vm.counter4 = 0;
+      }, 2000);
+    }
   },
   methods: {
     changeTitle: function(event) {
@@ -36,7 +56,8 @@ new Vue({
     //   this.counter4--;
     //   this.result4 = this.counter4 > 5 ? 'Greater 5' : 'Smaller 5';
     // },
-    result4() {
+    result4: function() {
+      // console.log('result')
       return this.counter4 > 5 ? 'Greater than 5' : 'Smaller than 5';
     },
     updateCoordinates: function(event) {
